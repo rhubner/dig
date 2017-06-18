c34e4ffd80ecb8a833a042736f90b909e8e64fa6
Radek Hubner <radek.hubner@gmail.com>
2017-06-18 12:15:42 +0100

    Pridan stav do komponenty, update tabulky v komponente.

--- client/src/main/scala/spatutorial/client/components/ExplorerTreeView.scala
+++ client/src/main/scala/spatutorial/client/components/ExplorerTreeView.scala
@@ -43,6 +43,10 @@ object ExplorerTreeView {
   case class Slozka(name : String) extends SoubSystem
   case class Soubor(name: String) extends SoubSystem
 
+  case class Props(tree: Tree[SoubSystem])
+  case class State(treeLoc: TreeLoc[SoubSystem])
+
+
   def slozka(name: String): SoubSystem = Slozka.apply(name)
   def soubor(name: String): SoubSystem = Soubor.apply(name)
 
@@ -59,117 +63,108 @@ object ExplorerTreeView {
       float.left,
       width(75.%%)
     )
-
   }
 
 
   def createTree() : Tree[SoubSystem] = {
     import scalaz.Scalaz._
-
     val strom: Tree[SoubSystem] =
       slozka("folder1").node(
-
         soubor("financial report.docx").leaf,
-
         slozka("dalsi subfolder").node(
           soubor("ahoj.txt").leaf,
           soubor("prdel.doc").leaf
         ),
-
-
         soubor("ahoj2.txt").leaf,
         slozka("empty node").node()
-
       )
-
     strom
-
   }
 
 
-  def createListItem(node: Tree[SoubSystem]): ReactElement = {
-    assert(node.rootLabel.isInstanceOf[Slozka], "Root element must be Slozka")
+  class Backend(t: BackendScope[Props, State]) {
 
-    //val subtree = forest.filter(!_.).map(x => createListItem(x)).toJsArray
-    val subtree = node.subForest.filter(x => x.rootLabel match {
-      case Slozka(_) => true
-      case _ => false
-    }).map(x => createListItem(x)).toJsArray
+    def itemCallBack(itemLoc: TreeLoc[SoubSystem])(reactEvent: ReactEvent) = {
+      t.modState(x => State(itemLoc)) >> Callback.log("message")
 
-    val subitems: js.UndefOr[js.Array[ReactElement]] = if(subtree.isEmpty) {
-       js.undefined
-    }else {
-      UndefOr.any2undefOrA(subtree)
     }
 
 
-    MuiListItem(key = node.rootLabel.name, primaryText = node.rootLabel.name, nestedItems = subitems,
+    def createListItem(node: Tree[SoubSystem]): ReactElement = {
+      assert(node.rootLabel.isInstanceOf[Slozka], "Root element must be Slozka")
+
+      //val subtree = forest.filter(!_.).map(x => createListItem(x)).toJsArray
+      val subtree = node.subForest.filter(x => x.rootLabel match {
+        case Slozka(_) => true
+        case _ => false
+      }).map(x => createListItem(x)).toJsArray
+
+      val subitems: js.UndefOr[js.Array[ReactElement]] = if(subtree.isEmpty) {
+        js.undefined
+      }else {
+        UndefOr.any2undefOrA(subtree)
+      }
+
+
+      MuiListItem(key = node.rootLabel.name, primaryText = node.rootLabel.name, nestedItems = subitems,
         leftIcon = FileFolder()(), initiallyOpen = true, onClick = UndefOr.any2undefOrA(itemCallBack(node.loc)))()
 
-  }
+    }
 
 
-  def itemCallBack(itemLoc: TreeLoc[SoubSystem])(reactEvent: ReactEvent) = Callback {
-    println("on lcick on element")
-    println("target : " + itemLoc.tree.rootLabel)
-  }
+    def createTable(treeLoc: TreeLoc[SoubSystem]): ReactElement = {
+      val files = treeLoc.tree.subForest.filter(x => x.rootLabel match {
+        case Soubor(f) => true
+        case _ => false
+      }).map(x => x.rootLabel.name)
 
 
-
-  def createTable(treeLoc: TreeLoc[SoubSystem]): ReactElement = {
-    val files = treeLoc.tree.subForest.filter(x => x.rootLabel match {
-      case Soubor(f) => true
-      case _ => false
-    }).map(x => x.rootLabel.name)
-
-
-    MuiTable(key="tabulka")(
-      MuiTableHeader(key="header")(
-        MuiTableRow(key="row1")(
-          MuiTableHeaderColumn(key="header1")("file neme"),
-          MuiTableHeaderColumn(key="header2")("puid"),
-          MuiTableHeaderColumn(key="header3")("closure")
-        )
-      ),
-      MuiTableBody(key="table body")(
-        files.map { file =>
-          MuiTableRow(key=file)(
-            MuiTableRowColumn(key="column1")(file),
-            MuiTableRowColumn(key="column2")("fmt/100"),
-            MuiTableRowColumn(key="column3")("open on transfer")
+      MuiTable(key="tabulka")(
+        MuiTableHeader(key="header")(
+          MuiTableRow(key="row1")(
+            MuiTableHeaderColumn(key="header1")("file neme"),
+            MuiTableHeaderColumn(key="header2")("puid"),
+            MuiTableHeaderColumn(key="header3")("closure")
           )
-        }
+        ),
+        MuiTableBody(key="table body")(
+          files.map { file =>
+            MuiTableRow(key=file)(
+              MuiTableRowColumn(key="column1")(file),
+              MuiTableRowColumn(key="column2")("fmt/100"),
+              MuiTableRowColumn(key="column3")("open on transfer")
+            )
+          }
+        )
       )
-    )
+    }
 
-  }
-
-
-
-  val component2 = ReactComponentB[Unit]("ExplorerTreeView")
-    .render(P => {
+    def render(p: Props, s: State) = {
       <.div("hello",
         MuiMuiThemeProvider()(
           <.div(
-          <.div(Style.leftMenu, ^.id:= "mujdiv")(
-          MuiList(key = "list1")(
-            createListItem(createTree())
+            <.div(Style.leftMenu, ^.id:= "mujdiv")(
+              MuiList(key = "list1")(
+                createListItem(p.tree)
+              )
+            ),//div pro menu end
+            <.div(Style.fileTable, ^.id:= "dabulkaDiv")(
+              createTable(s.treeLoc)
+            )
+          )//div pro mui container
         )
-
-      ),//div pro menu end
-        <.div(Style.fileTable, ^.id:= "dabulkaDiv")(
-            createTable(createTree().loc)
-        )
+      )
+    }
+  }
 
 
-    )//div pro mui container
+  val component = ReactComponentB[Props]("ExplorerTreeView")
+    .initialState_P(p => State(p.tree.loc))
+    .renderBackend[Backend]
+    .build
 
-    )
-  )
-}).build
 
-
-  def apply() = component2()
+  def apply() = component(Props(createTree()))
 
 
 }
